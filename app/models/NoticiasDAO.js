4845ff08a1d23a35fb0674d17d33138996bbd97e
lucas_petrolli <lucas.almeida@somoskratos.com.br>
2020-01-08 14:08:08 -0400

    Adiciona validação das informações do formulario

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -14,7 +14,7 @@ NoticiasDAO.prototype.getNoticia = function(callback) {
 };
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback) {
-  this._connection.query('insert into noticias set ?', noticia, callback);
+  this._connection.query('INSERT INTO noticias SET ?', noticia, callback);
 };
 
 module.exports = () => NoticiasDAO;
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,29 @@
 module.exports = function(application) {
   application.get('/formulario_inclusao_noticia', function(req, res) {
-    res.render('admin/form_add_noticia');
+    res.render('admin/form_add_noticia', { validacao: {} });
   });
 
   application.post('/noticias/salvar', function(req, res) {
     var noticia = req.body;
+
+    req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+    req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+    req
+      .assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres')
+      .len(10, 100);
+    req.assert('autor', 'Autor é obrigatório').notEmpty();
+    req
+      .assert('data', 'Data é obrigatório')
+      .notEmpty()
+      .isDate({ format: 'YYYY-MM-DD' });
+    req.assert('noticia', 'Notcia é obrigatório').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+      res.render('admin/form_add_noticia', { validacao: erros });
+      return;
+    }
 
     var connection = application.config.dbConnection();
     var NoticiasDAO = new application.app.models.NoticiasDAO(connection);
