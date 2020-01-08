49b18b584f068ec1dd565aff790f897b6345a4e3
lucas_petrolli <lucas.almeida@somoskratos.com.br>
2020-01-08 17:31:11 -0400

    Cria controller para noticia e move a logira do controller que estava na rota para o controller

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,19 +1,9 @@
 module.exports = function(application) {
   application.get('/noticias', function(req, res) {
-    var connection = application.config.dbConnection();
-    var NoticiasDAO = new application.app.models.NoticiasDAO(connection);
-
-    NoticiasDAO.getNoticias(function(error, result) {
-      res.render('noticias/noticias', { noticias: result });
-    });
+    application.app.controllers.noticias.noticias(application, req, res);
   });
 
   application.get('/noticia', function(req, res) {
-    var connection = application.config.dbConnection();
-    var NoticiasDAO = new application.app.models.NoticiasDAO(connection);
-
-    NoticiasDAO.getNoticia(function(error, result) {
-      res.render('noticias/noticia', { noticia: result });
-    });
+    application.app.controllers.noticias.noticia(application, req, res);
   });
 };
